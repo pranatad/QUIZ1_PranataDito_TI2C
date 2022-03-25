cd21b4f78f4e7c121b64f78ed236992c14b30121
pranatad <2041720043@student.polinema.ac.id>
2022-03-25 15:48:57 +0700

    QUIZ1_Pranata DIto Fitrityansyah_TI2C

---
+++ resources/views/layouts/inc/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+                <!-- header inner -->
+                <div class="header">
+
+                    <div class="container-fluid">
+
+                        <div class="row">
+                            <div class="col-lg-3 logo_section">
+                                <div class="full">
+                                    <div class="center-desk">
+                                        <div class="logo">
+                                            <a href="index.html"><img src="images/logo2.jpg" alt="#"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-9">
+                                <div class="right_header_info">
+                                    <ul>
+                                        <li>
+                                            <a href="#"><img style="margin-right: 15px;" src="icon/1.png" alt="#" /></a>
+                                        </li>
+                                        <li class="tytyu">
+                                            <a href="#"><img style="margin-right: 15px;" src="icon/2.png" alt="#" /></a>
+                                        </li>
+                                        <li>
+                                            <a href="#"><img style="margin-right: 15px;" src="icon/3.png" alt="#" /></a>
+                                        </li>
+
+                                        <li>
+                                            <button type="button" id="sidebarCollapse">
+                                                <img src="images/menu_icon.png" alt="#" />
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- end header inner -->
+            </header>
